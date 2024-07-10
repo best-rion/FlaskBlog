55b9bce977e0696f53bcce8dd539c124e29a28ca
MD. Hossain <husayn.buet@gmail.com>
2024-07-11 00:32:34 +0600

    .gitignore added. Now ready to push.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 class Config:
-    SECRET_KEY = '2b2a6e492a824a6d844b9af527c9b415'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.getenv('SECRET_KEY') #example : 'rljr4yawodg4f4f6fovbw2y8cni8889u'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') #example : 'sqlite:///example.db'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    load_dotenv()
-    MAIL_USERNAME = os.getenv('USERNAME')
-    MAIL_PASSWORD = os.getenv('PASSWORD')
+    MAIL_USERNAME = os.getenv('USERNAME') #example : 'example@demo.com'
+    MAIL_PASSWORD = os.getenv('PASSWORD') #example : '44gg 806q eoq3 jjoh'
